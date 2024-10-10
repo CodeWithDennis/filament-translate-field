1dec0db84f424edd15b6daf0dcac9a1c2b4090a0
Dennis Elsinga <personal.elsinga@gmail.com>
2024-10-10 11:39:58 +0200

    Update translation handling in Filament with placeholders

--- config/filament-translate-field.php
+++ config/filament-translate-field.php
@@ -1,7 +1,29 @@
 <?php
 
 return [
+
+    /*
+     * Automatically detect the language of the text to translate.
+     *
+     * Whether to automatically detect the language of the text to translate.
+     */
+
     'auto_detect' => true,
+
+    /*
+     * The default language to translate to.
+     *
+     * This should be the key of the language in the languages array.
+     */
+
+    'default_language' => 'en',
+
+    /*
+     * The languages that the user can translate to.
+     *
+     * The key should be the language code and the value should be the language name.
+     */
+
     'languages' => [
         'en' => 'English',
     ],
--- src/Filament/Forms/Actions/TranslateAction.php
+++ src/Filament/Forms/Actions/TranslateAction.php
@@ -5,8 +5,9 @@ namespace CodeWithDennis\FilamentTranslateField\Filament\Forms\Actions;
 use Closure;
 use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Get;
 use Filament\Forms\Set;
 use Stichoza\GoogleTranslate\GoogleTranslate;
@@ -34,7 +35,7 @@ class TranslateAction extends Action
         return function (array $data, Set $set) {
             $column = $this->getComponent()->getStatePath(false);
 
-            $set($column, $data['translation']);
+            $set($column, $data['translated']);
         };
     }
 
@@ -64,35 +65,44 @@ class TranslateAction extends Action
                         ->columns()
                         ->live(),
                     Select::make('language')
-                        ->afterStateUpdated(function (Set $set, Get $get, $state, GoogleTranslate $googleTranslate) {
-                            if (! empty($state)) {
-                                $googleTranslate->setTarget($state);
-                                $googleTranslate->setSource(! config('filament-translate-field.auto_detect') ? $get('original_language') : null);
-
-                                $component = $this->getComponent();
-                                $field = $component->getRecord()->{$component->getStatePath(false)};
-
-                                $set('original', $field);
-                                $set('translation', $googleTranslate->translate($field));
-                            } else {
-                                $set('translation', null);
-                            }
-                        })
+                        ->default(fn () => config('filament-translate-field.default_language'))
                         ->options($this->getLanguages())
                         ->required()
                         ->live(),
                 ]),
-            Textarea::make('original')
+            Placeholder::make('original')
                 ->label(__('Original'))
-                ->dehydrated(false)
-                ->disabled()
-                ->visible(fn (Get $get) => ! empty($get('language')))
-                ->rows(5)
-                ->required(),
-            Textarea::make('translation')
+                ->content(function () {
+                    $component = $this->getComponent();
+
+                    return $component->getRecord()->{$component->getStatePath(false)};
+                })
+                ->visible(fn (Get $get) => ! empty($get('language'))),
+            Placeholder::make('translation')
                 ->label(__('Translation'))
+                ->live()
                 ->visible(fn (Get $get) => ! empty($get('language')))
-                ->rows(5),
+                ->content(function (Get $get, Set $set, GoogleTranslate $googleTranslate) {
+                    $language = $get('language');
+
+                    if (! empty($language)) {
+                        $googleTranslate->setTarget($language);
+                        $googleTranslate->setSource(! config('filament-translate-field.auto_detect') ? $get('original_language') : null);
+
+                        $component = $this->getComponent();
+                        $field = $component->getRecord()->{$component->getStatePath(false)};
+                        $translation = $googleTranslate->translate($field);
+
+                        $set('translated', $translation);
+
+                        return $translation;
+                    }
+
+                    $set('translated', null);
+
+                    return null;
+                }),
+            Hidden::make('translated'),
         ]);
 
         $this->action('translate');
